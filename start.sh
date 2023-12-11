6c909d9aefa59734b2038143d30aff71ad8d7d75
chalathorn prasongtham <chalathorn.pra@bkp.co.th>
2023-12-11 14:49:29 +0700

    update

--- start.sh
+++ start.sh
@@ -1,80 +1,40 @@
-#!/bin/bash
+kind create cluster --config kind/config.yaml --name minio
+kubectl apply -f kind/ingress-controller.yaml
+sleep 10
+kubectl wait --namespace ingress-nginx \
+  --for=condition=ready pod \
+  --selector=app.kubernetes.io/component=controller \
+  --timeout=140s
 
-if ! [ -x "$(command -v docker-compose)" ]; then
-  echo 'Error: docker-compose is not installed.' >&2
-  exit 1
-fi
-
-domains=(dev.file.hosting.jumpai.tech)
-rsa_key_size=4096
-data_path="./certbot"
-email="cprasongtham@snaplogic.com" # Adding a valid address is strongly recommended
-staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
-
-if [ -d "$data_path" ]; then
-  read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
-  if [ "$decision" != "Y" ] && [ "$decision" != "y" ]; then
-    exit
-  fi
-fi
+helm repo add jetstack https://charts.jetstack.io
+helm install \
+  cert-manager jetstack/cert-manager \
+  --namespace cert-manager \
+  --create-namespace \
+  --version v1.13.2 \
+  --set installCRDs=true
 
 
-if [ ! -e "$data_path/conf/options-ssl-nginx.conf" ] || [ ! -e "$data_path/conf/ssl-dhparams.pem" ]; then
-  echo "### Downloading recommended TLS parameters ..."
-  mkdir -p "$data_path/conf"
-  curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot-nginx/certbot_nginx/_internal/tls_configs/options-ssl-nginx.conf > "$data_path/conf/options-ssl-nginx.conf"
-  curl -s https://raw.githubusercontent.com/certbot/certbot/master/certbot/certbot/ssl-dhparams.pem > "$data_path/conf/ssl-dhparams.pem"
-  echo
-fi
+# helm install -n cert-manager namecheap-webhook cert-manager-webhook-namecheap/deploy/cert-manager-webhook-namecheap/
 
-echo "### Creating dummy certificate for $domains ..."
-path="/etc/letsencrypt/live/$domains"
-mkdir -p "$data_path/conf/live/$domains"
-docker-compose run --rm --entrypoint "\
-  openssl req -x509 -nodes -newkey rsa:$rsa_key_size -days 1\
-    -keyout '$path/privkey.pem' \
-    -out '$path/fullchain.pem' \
-    -subj '/CN=localhost'" certbot
-echo
+# helm install --set email=cprasonghtam@snaplogic.com -n cert-manager letsencrypt-namecheap-issuer cert-manager-webhook-namecheap/deploy/letsencrypt-namecheap-issuer/
+
+kubectl create secret generic namecheap-credentials \
+  --namespace=cert-manager \
+  --from-literal=apiKey=test \
+  --from-literal=apiUser=test
 
 
-echo "### Starting nginx ..."
-docker-compose up --force-recreate -d nginx
-echo
+kubectl create ns minio
+# kubectl apply -f cert-manager-webhook-namecheap/certificate.yaml
 
-echo "### Deleting dummy certificate for $domains ..."
-docker-compose run --rm --entrypoint "\
-  rm -Rf /etc/letsencrypt/live/$domains && \
-  rm -Rf /etc/letsencrypt/archive/$domains && \
-  rm -Rf /etc/letsencrypt/renewal/$domains.conf" certbot
-echo
+kubectl create secret generic minio-root-credential \
+  --namespace=minio \
+  --from-literal=root-user=admin \
+  --from-literal=root-password=gfkgvkgft
 
+kubectl apply -f minio/pesistence.yaml -n minio
 
-echo "### Requesting Let's Encrypt certificate for $domains ..."
-#Join $domains to -d args
-domain_args=""
-for domain in "${domains[@]}"; do
-  domain_args="$domain_args -d $domain"
-done
+helm repo add bitnami https://charts.bitnami.com/bitnami
+helm upgrade --install minio bitnami/minio -f minio/values/minio-values.yaml -n minio
 
-# Select appropriate email arg
-case "$email" in
-  "") email_arg="--register-unsafely-without-email" ;;
-  *) email_arg="--email $email" ;;
-esac
-
-# Enable staging mode if needed
-if [ $staging != "0" ]; then staging_arg="--staging"; fi
-
-docker-compose run --rm --entrypoint "\
-  certbot certonly --webroot -w /var/www/certbot \
-    $staging_arg \
-    $email_arg \
-    $domain_args \
-    --rsa-key-size $rsa_key_size \
-    --agree-tos \
-    --force-renewal" certbot
-echo
-
-echo "### Reloading nginx ..."
-docker-compose exec nginx nginx -s reload
